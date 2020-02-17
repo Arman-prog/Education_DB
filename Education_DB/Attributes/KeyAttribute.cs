708aebe4feebb7c20f9c660764ab1955f55ed4bf
Arman Harutyunyan <aramich11@gmail.com>
2020-02-17 14:55:20 +0400

    add Attributes

---
+++ Education_DB/Attributes/KeyAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Education_DB.Attributes
+{
+    public class KeyAttribute : Attribute
+    {
+    }
+}
---
+++ Education_DB/Attributes/TableNameAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Education_DB.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class TableNameAttribute : Attribute
+    {
+        public TableNameAttribute(string tableName)
+        {
+            TableName = tableName;
+        }
+
+        public string TableName { get; private set; }
+    }
+}
